76e598571dba5a9fee7dcbde82d3601233ceeed4
anitapm03 <apereiramolinero@gmail.com>
2024-04-18 09:38:13 +0200

    Add project files.

---
+++ ConsoleCacheRedis/HelperCacheMultiplexer.cs
@@ -0,0 +1,29 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCacheRedis
+{
+    public class HelperCacheMultiplexer
+    {
+        private static Lazy<ConnectionMultiplexer>
+            CreateConnection = new Lazy<ConnectionMultiplexer>
+            (() =>
+            {
+                string cacheRedisKeys = "cacheredisanita.redis.cache.windows.net:6380,password=pJHOCAW4Ukgut03dQfNd3Cgoqs8sMtnETAzCaNv0MPg=,ssl=True,abortConnect=False";
+                return ConnectionMultiplexer.Connect(cacheRedisKeys);
+            });
+
+        public static ConnectionMultiplexer Connection
+        {
+            get
+            {
+                return CreateConnection.Value;
+            }
+        }
+
+    }
+}
---
+++ ConsoleCacheRedis/Program.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleCacheRedis;
+
+Console.WriteLine("Test Cache Redis");
+
+ServiceCacheRedis service = new ServiceCacheRedis();
+List<Producto> favoritos = await
+   service.GetProductosFavortos();
+
+if (favoritos == null)
+{
+    Console.WriteLine("no hay favs");
+}
+else
+{
+    int i = 1;
+    foreach(Producto p in favoritos)
+    {
+        Console.WriteLine(i + " - " + p.Nombre);
+        i += 1;
+    }
+}
+Console.WriteLine("yata");
